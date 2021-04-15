a3a1468c62230d8bf9fc4350674582534a6e8af9
CGalko <cash.galko12@ncf.edu>
2021-04-14 20:50:59 -0400

    Added the inquirer prompt & most if not all of the questions necessary to gather the info needed to generate the README. created a simple function to create a test file to test the application. Began using the 'when' functionality of inquirer.

--- index.js
+++ index.js
@@ -1,16 +1,96 @@
-// TODO: Include packages needed for this application
+
 
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = ['Please enter the title of your project.', 'Please enter a detailed description of your project.', 'Please include any relevent installation instructions.', 'Please enter any useage information needed by the user.', 'Is this project open source?', 'Please enter any guidelines for contributors to your project or provide a link to your contributing.md file.', 'Please enter detailed testing instructions.', 'Please choose the correct license for this application.'];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Invokes the inquirer npm and lists the necessary questions. Then generates the README file with the data collected from the questions. 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'Please enter the title of your project.',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      'Please enter a detailed description of your project.',
+      name: 'descript',
+      
+    },
+    {
+      type: 'input',
+      message: 'Please include any relevent installation instructions.',
+      name: 'install',
+      
+    },
+    {
+      type: 'input',
+      message: 'Please enter any useage information needed by the user.',
+      name: 'usage',
+      
+    },
+    {
+      type: 'confirm',
+      message: 'Is your project open sourced?',
+      name: 'openSource',
+      
+    },
+    {
+      type: 'confirm',
+      message: 'Do you have a contribution.md?',
+      name: 'contribution',
+      when: (answers) => answers.openSource === true,
+    },
+    {
+      type: 'input',
+      message: 'Please provide a link to your contribution.md.',
+      name: 'contribFile',
+      when: (answers) => answers.contribution === true,
+    },
+    {
+      type: 'input',
+      message: 'Please enter any guidelines for contributors to your project.',
+      name: 'contribGuide',
+      when: (answers) => answers.contribFile === false || answers.openSource === true,
+    },
+    {
+      type: 'input',
+      message: 'Please enter detailed testing instructions.',
+      name: 'testInfo',
+        
+    },
+    {
+      type: 'input',
+      message: 'What is your Github username?',
+      name: 'gitUser',
+        
+    },
+    {
+      type: 'input',
+      message: 'Please provide the URL to your Github Profile.',
+      name: 'gitURL',
+        
+    },
+    {
+      type: 'list',
+      message: 'Please choose the correct license for this application.',
+      name: 'license',
+      choices: ['1', '2', '3']
+    },
 
-// TODO: Create a function to initialize app
-function init() {}
+  ])
+  .then((data) => {
+    const filename = `README.md`;
+    const readMeTemp = `${data.title}`
+    fs.writeFile(filename, readMeTemp, (err) => 
+    err ? console.log(err) : console.log('Success!')
 
-// Function call to initialize app
-init();
+    );
+  });
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
