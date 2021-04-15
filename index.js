79e1c6eb6289ef7d80d13c14723a941dbb0f1f8a
CGalko <cash.galko12@ncf.edu>
2021-04-14 20:05:19 -0400

    Added the list of questions that this application will cover into the questions array.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['Please enter the title of your project.', 'Please enter a detailed description of your project.', 'Please include any relevent installation instructions.', 'Please enter any useage information needed by the user.', 'Is this project open source?', 'Please enter any guidelines for contributors to your project or provide a link to your contributing.md file.', 'Please enter detailed testing instructions.', 'Please choose the correct license for this application.'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
